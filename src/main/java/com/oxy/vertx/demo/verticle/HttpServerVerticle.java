e1ccc071ec3795f783cadc7979f9f1d40b46dc79
hoangtn <hoangtn@vng.com.vn>
2021-05-12 09:56:38 +0700

    update

--- src/main/java/com/oxy/vertx/demo/verticle/HttpServerVerticle.java
+++ src/main/java/com/oxy/vertx/demo/verticle/HttpServerVerticle.java
@@ -19,7 +19,7 @@ public class HttpServerVerticle extends AbstractVerticle {
         log.warn("Http server is starting...");
         HttpServer server = vertx.createHttpServer();
         Router router = Router.router(vertx);
-        server.requestHandler(router).listen(8008);
+        server.requestHandler(router).listen(8007);
         router.route().handler(BodyHandler.create());
         routing(router);
         log.info("Start http server success");
