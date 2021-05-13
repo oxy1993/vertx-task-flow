3a43026e19723fedf3241cc7076122797b2b5de4
hoangtn <hoangtn@vng.com.vn>
2021-05-13 09:08:57 +0700

    update

--- src/main/java/com/oxy/vertx/Main.java
+++ src/main/java/com/oxy/vertx/Main.java
@@ -25,7 +25,7 @@ public class Main {
         Vertx.clusteredVertx(options, res -> {
             if (res.succeeded()) {
                 Vertx vertx = res.result();
-                DeploymentOptions deploymentOptions = new DeploymentOptions().setInstances(3);
+                DeploymentOptions deploymentOptions = new DeploymentOptions().setInstances(9);
                 vertx.deployVerticle(HttpServerVerticle.class.getName(), deploymentOptions);
             }
         });
