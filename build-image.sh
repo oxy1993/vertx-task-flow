51c6b30bdbfdb041451c5c3fae4f604443f89c72
hoangtn <hoangtn@vng.com.vn>
2021-05-13 10:10:07 +0700

    update

--- build-image.sh
+++ build-image.sh
@@ -1,1 +1,1 @@
-mvn clean package && docker build -t vertx-flow-task . && docker image tag vertx-flow-task registry.unipass.vn/vertx-flow-task:v1.7.1 && docker push registry.unipass.vn/vertx-flow-task:v1.7.1
+mvn clean package && docker build -t vertx-flow-task . && docker image tag vertx-flow-task registry.unipass.vn/vertx-flow-task:v1.8.1 && docker push registry.unipass.vn/vertx-flow-task:v1.8.1
