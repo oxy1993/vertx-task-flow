10bc5535e798109c20f0e50cf2325117e71813c4
hoangtn <hoangtn@vng.com.vn>
2021-05-14 13:54:25 +0700

    update

--- src/main/java/com/oxy/vertx/demo/verticle/HttpServerVerticle.java
+++ src/main/java/com/oxy/vertx/demo/verticle/HttpServerVerticle.java
@@ -25,7 +25,7 @@ public class HttpServerVerticle extends AbstractVerticle {
         Router router = routing();
         vertx.createHttpServer()
             .requestHandler(router)
-            .listen(8028, http -> {
+            .listen(8029, http -> {
                 if (http.succeeded()) {
                     new LoadConfigFlow().run(new StartUpMsg(), done -> {
                         if (!done.isBreakWorkFlow()) {
