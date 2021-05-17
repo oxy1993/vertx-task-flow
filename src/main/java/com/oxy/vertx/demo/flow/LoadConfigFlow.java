02213d5e9045c4dd4116c414bd69881c7fad51ad
hoangtn <hoangtn@vng.com.vn>
2021-05-17 18:30:38 +0700

    update

--- src/main/java/com/oxy/vertx/demo/flow/LoadConfigFlow.java
+++ src/main/java/com/oxy/vertx/demo/flow/LoadConfigFlow.java
@@ -5,6 +5,9 @@ import com.oxy.vertx.base.WarriorTask;
 import com.oxy.vertx.base.entities.StartUpMsg;
 import com.oxy.vertx.demo.task.start_up.StartHttpServerTask;
 
+import javax.inject.Singleton;
+
+@Singleton
 public class LoadConfigFlow extends BaseStartUpFlow {
     @Override
     protected WarriorTask<StartUpMsg> getChildTask() {
